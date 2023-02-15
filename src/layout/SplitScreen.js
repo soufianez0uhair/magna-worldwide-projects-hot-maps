02f09f6474cb1d1afdeadce296208c64e0538465
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-02-15 12:06:11 +0100

    Create Split Screen layout component

---
+++ src/layout/SplitScreen.js
@@ -0,0 +1,15 @@
+function SplitScreen({children}) {
+  const [Left, Right] = children;
+  return (
+    <div className="SplitScreen">
+      <div className="SplitScreen__left">
+        <Left />
+      </div>
+      <div className="SplitScreen__right">
+        <Right />
+      </div>
+    </div>
+  )
+}
+
+export default SplitScreen;
